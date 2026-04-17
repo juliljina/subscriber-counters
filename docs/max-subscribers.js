a4c0c12dd9a72bbddb01e4dd517f4f45e25a158f
juliljina <juliljina@gmail.com>
2026-04-17 20:41:54 +0300

    Add generated MAX subscriber data script

---
+++ docs/max-subscribers.js
@@ -0,0 +1,18 @@
+window.MaxSubscriberCounts = {
+  "version": 1,
+  "generatedAt": "2026-04-17T17:33:13.786Z",
+  "channels": {
+    "id782615006635_biz": {
+      "ok": true,
+      "id": "id782615006635_biz",
+      "title": "Гранты стартапам",
+      "url": "https://max.ru/id782615006635_biz",
+      "count": 163,
+      "formatted": "163 подписчика",
+      "updatedAt": "2026-04-17T17:33:13.786Z",
+      "source": "max",
+      "stale": false
+    }
+  }
+};
+window.dispatchEvent(new CustomEvent('max-subscribers:loaded', { detail: window.MaxSubscriberCounts }));
